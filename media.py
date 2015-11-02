c7ddb5260ac1378cdb955f095eca697c337b1263
Chris J <christopher.w.jarvis10@gmail.com>
2015-11-02 12:00:06 -0700

    add year to info, will probably have to adjust fresh tomatoes as well

--- media.py
+++ media.py
@@ -1,10 +1,11 @@
 import webbrowser
 class Movie():
 	'''allows movie information to be stored for each movie'''
-	def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
+	def __init__(self, movie_title, poster_image_url, trailer_youtube_url, year):
 		self.title = movie_title
 		self.poster_image_url = poster_image_url
 		self.trailer_youtube_url = trailer_youtube_url
+		self.year = year
 
 	def open_trailer():
 		webbrowser.open(self.trailer)
--- my_movies.py
+++ my_movies.py
@@ -6,22 +6,28 @@ import fresh_tomatoes
 
 blood_diamond = media.Movie('Blood Diamond',
 	'https://upload.wikimedia.org/wikipedia/en/5/5a/Blooddiamondposter.jpg',
-	'https://youtu.be/yknIZsvQjG4')
+	'https://youtu.be/yknIZsvQjG4',
+	'2006')
 interstellar = media.Movie('Interstellar',
 	'https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg',
-	'https://youtu.be/2LqzF5WauAw')
+	'https://youtu.be/2LqzF5WauAw',
+	'2014')
 nacho_libre = media.Movie('Nacho Libre',
 	'https://upload.wikimedia.org/wikipedia/en/3/35/Nachopost.jpg',
-	'https://youtu.be/ElVSw6xpQ70')
+	'https://youtu.be/ElVSw6xpQ70',
+	'2006')
 man_on_fire = media.Movie('Man on Fire',
 	'https://upload.wikimedia.org/wikipedia/en/e/e8/Man_on_fireposter.jpg',
-	'https://youtube/g4kLizDXLY0')
+	'https://youtube/g4kLizDXLY0',
+	'2004')
 act_of_killing = media.Movie('The Act of Killing',
 	'https://upload.wikimedia.org/wikipedia/en/c/ca/The_Act_of_Killing_%282012_film%29.jpg',
-	'https://youtu.be/SD5oMxbMcHM')
+	'https://youtu.be/SD5oMxbMcHM',
+	'2013')
 casino_royale = media.Movie('Casino Royale',
 	'https://upload.wikimedia.org/wikipedia/en/1/15/Casino_Royale_2_-_UK_cinema_poster.jpg',
-	'https://youtu.be/fl5WHj0bZ2Q')
+	'https://youtu.be/fl5WHj0bZ2Q',
+	'2006')
 
 #put movies in list to use with fresh tomatoes method
 
