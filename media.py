0bc7164c0deb2bf770371c26933e366c9dc7a1cb
Chris J <christopher.w.jarvis10@gmail.com>
2015-11-04 13:00:37 -0700

    First commit for new branch, this adds other kinds of media to display

--- media.py
+++ media.py
@@ -1,11 +1,35 @@
 import webbrowser
-class Movie():
-	'''allows movie information to be stored for each movie'''
-	def __init__(self, movie_title, poster_image_url, trailer_youtube_url, year):
-		self.title = movie_title
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
+class Media():
+	'''store basic information for general medium type'''
+	def __init__(self, title, image_url, description, year):
+		self.title = title
+		self.art = image_url
+		self.description = description
 		self.year = year
+
+class Movie(Media):
+	'''film inherits from Media, adding a trailer, cast and a method;
+	cast should be the three main actors'''
+	def __init__(self,title, image_url, description, year, trailer_url, 
+	cast):
+		Media.__init__(self, title, image_url, description, year)
+		self.trailer = trailer_url
+		self.cast = cast
 
 	def open_trailer():
 		webbrowser.open(self.trailer)
+
+class Television(Media, Movie):
+	'''inherits from Media and the trailer attribute from Movie, adds
+	a network attribute'''
+	def __init__(self, title, image_url, description, year, cast, network):
+		Media.__init__(self, title, image_url, description, year)
+		self.cast = cast
+		self.network = network
+
+class Book(Media):
+	def __init__(self, title, image_url, description, year, author):
+		Media.__init__(self, title, image_url, description, year)
+		self.author = author
+
+
--- my_movies.py
+++ my_movies.py
@@ -4,44 +4,92 @@ import fresh_tomatoes
 # this is where all my movies and their information is stored, using the Movie
 # class structure from the media.py file
 
-blood_diamond = media.Movie(
-        'Blood Diamond',
-	'https://upload.wikimedia.org/wikipedia/en/5/5a/Blooddiamondposter.jpg',
-	'https://youtu.be/yknIZsvQjG4',
-	'2006')
-interstellar = media.Movie(
-        'Interstellar',
-	'https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg', #noqa
-	'https://youtu.be/2LqzF5WauAw',
-	'2014')
-nacho_libre = media.Movie(
-        'Nacho Libre',
-	'https://upload.wikimedia.org/wikipedia/en/3/35/Nachopost.jpg',
-	'https://youtu.be/ElVSw6xpQ70',
-	'2006')
-man_on_fire = media.Movie(
-        'Man on Fire',
-	'https://upload.wikimedia.org/wikipedia/en/e/e8/Man_on_fireposter.jpg',
-	'https://youtube/g4kLizDXLY0',
-	'2004')
-act_of_killing = media.Movie(
-        'The Act of Killing',
-	'https://upload.wikimedia.org/wikipedia/en/c/ca/The_Act_of_Killing_%282012_film%29.jpg', #noqa
-	'https://youtu.be/SD5oMxbMcHM',
-	'2013')
-casino_royale = media.Movie(
-        'Casino Royale',
-	'https://upload.wikimedia.org/wikipedia/en/1/15/Casino_Royale_2_-_UK_cinema_poster.jpg', #noqa
-	'https://youtu.be/fl5WHj0bZ2Q',
-	'2006')
+# Movies
 
+blood_diamond = media.Movie(
+    'Blood Diamond',
+	'https://upload.wikimedia.org/wikipedia/en/5/5a/Blooddiamondposter.jpg',
+	'A mercenary helps a man find his family in the midst of diamond fueled'
+	' civil war',
+	'2006',
+	'https://youtu.be/yknIZsvQjG4',
+	'Leonardo Dicaprio, Djimon Honsou, Jennifer Connelly')
+interstellar = media.Movie(
+    'Interstellar',
+	'https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg', #noqa
+	'A man leaves his family in a desperate attempt to find another planet'
+	' suitable for life',
+	'2014',
+	'https://youtu.be/2LqzF5WauAw',
+	'Matthew McConaughey, Anne Hatheway, Matt Damon')
+nacho_libre = media.Movie(
+    'Nacho Libre',
+	'https://upload.wikimedia.org/wikipedia/en/3/35/Nachopost.jpg',
+	'A lowly monk chases his dream of fighting in the ring',
+	'2006',
+	'https://youtu.be/ElVSw6xpQ70',
+	'Jack Black, Ana de la Reguera')
+man_on_fire = media.Movie(
+    'Man on Fire',
+	'https://upload.wikimedia.org/wikipedia/en/e/e8/Man_on_fireposter.jpg',
+	'A broken former CIA operative becomes a bodyguard',
+	'2004',
+	'https://youtube/g4kLizDXLY0',
+	'Denzel Washington, Dakota Fanning, Christopher Walken')
+casino_royale = media.Movie(
+    'Casino Royale',
+	'https://upload.wikimedia.org/wikipedia/en/1/15/Casino_Royale_2_-_UK_cinema_poster.jpg', #noqa
+	'James Bond battles villain Le Chiffre in a high-stakes poker game',
+	'2006',
+	'https://youtu.be/fl5WHj0bZ2Q',
+	'Daniel Craig, Judi Dench, Eva Green')
+
+# TV Shows
+
+ray_donovan = media.Television(
+	'Ray Donovan',
+	'http://www.gstatic.com/tv/thumb/tvbanners/9311951/p9311951_b_v7_ae.jpg',
+	'Ray Donovan fixes problems for LA\'s wealthy elite but no amount'
+	' of money and power is enough to hide from his troubled past',
+	'2013',
+	'Leiv Schreiber, John Voight, Stephen Bauer',
+	'Showtime')
+bored_to_death = media.Television(
+	'Bored to Death',
+	'http://www.gstatic.com/tv/thumb/tvbanners/3621786/p3621786_b_v7_ab.jpg',
+	'A bumbling writer decides to change his life and moonlight as'
+	'as a private detective',
+	'2009',
+	'Jason Schwartzman, Ted Danson, Zach Galifinakis',
+	'HBO')
+
+# Books
+
+blood_meridian = media.Book(
+	'Blood Meridian',
+	'http://t3.gstatic.com/images?q=tbn:ANd9GcQU9dyQxn4l6kSpYw47jOueuC-3Kn0ZI9FBOfLGpyDhvfEnClOz', #noqa
+	'A teenager stumbles into the violent events surrounding the Texas'
+	'-Mexico border in the 1850\'s',
+	'1985',
+	'Cormac McCarthy')
+fountainhead = media.Book(
+	'The Fountainhead',
+	'http://t3.gstatic.com/images?q=tbn:ANd9GcQ6L-0cbVEE7XRuAHIL1INcj74VUMKK63JRb_zFZcWgzCP9m9Jm', #noqa
+	'A philophical fictional story asking the question \'Can man\'s ego'
+	' be the fountainhead for human progress?\'',
+	'1943',
+	'Ayn Rand')
 # put movies in list to use with fresh tomatoes method
 
-movies_list = [blood_diamond,interstellar,nacho_libre,man_on_fire,
-               act_of_killing,casino_royale]
+movie_list = [blood_diamond,interstellar,nacho_libre,man_on_fire,
+casino_royale]
+
+tv_list = [ray_donovan, bored_to_death]
+
+book_list = [blood_meridian, fountainhead]
 
 # create website using fresh tomatoes module and my list of movies
 
-fresh_tomatoes.open_movies_page(movies_list)
+fresh_tomatoes.open_movies_page(movie_list)
 
 
